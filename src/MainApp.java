3299dde679fbfc9a1870b47d36312024625c7a6e
Kristen Carethers <kcarethers@gmail.com>
2016-09-19 15:08:49 -0400

    Created multi array statement

--- src/MainApp.java
+++ src/MainApp.java
@@ -1,9 +1,34 @@
+import java.util.Scanner;
 
 public class MainApp {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Scanner input = new Scanner(System.in);
+		int x = input.nextInt();
+		//String [] roster= new String[2]; //may need to adjust number
+		String[][] roster = new String [5][3]; //create a multi array statement.  type[] [] arrayName=new type[rowCount][columnCount];
+		
+		
+		//Sysout Welcome and ask who you want to learn more about
+		System.out.println("Welcome to our Java class.  Which student would you like to learn more about? (enter a number 1 -5):");
+		
+		String[][] roster = {{"Armani", "Music Studio", "Dubai"}, {"Elle", "Money Printing Factory", "The Future"}, 
+			{"Dominique", "Holistic Nail Bar", "Cuba"}, {"Daniel", "Bank", "Hong Kong"} {"Zalika", "Bakery", "Egypt"}};
+		
+		
+		
+//		Object[][] rosterName;
+//		Object[][] rosterBusiness;
+//		Object[][] rosterTravel;
+//		rosterName [x] [0] = 1;
+//		rosterBusiness [x] [1] = 2;
+//		rosterTravel [x] [2] = 3; 
+//		
+		
+		
+		}//psvm
 
-	}
+	}//class
 
-}
+
