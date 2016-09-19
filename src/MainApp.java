84f7482c6f13900e23d1baf71fac91705f5c49c2
Kristen Carethers <kcarethers@gmail.com>
2016-09-19 16:12:41 -0400

    Added statementthat told the Students name and asked if they wanted to know their dream job or travel

--- src/MainApp.java
+++ src/MainApp.java
@@ -7,18 +7,22 @@ public class MainApp {
 		
 		
 		//Sysout Welcome and ask who you want to learn more about
-		System.out.println("Welcome to our Java class.  Which student would you like to learn more about? (enter a number 1 -5):");
+		//System.out.println("Welcome to our Java class.  Which student would you like to learn more about? (enter a number 1 -5):");
 		
 		String[][] roster = {{"Armani", "Music Studio", "Dubai"}, {"Elle", "Money Printing Factory", "The Future"}, 
 			{"Dominique", "Holistic Nail Bar", "Cuba"}, {"Daniel", "Bank", "Hong Kong"}, {"Zalika", "Bakery", "Egypt"}};
 		
 		Scanner input = new Scanner(System.in);
-		System.out.println("Which student would you like learn more about?");
+		System.out.println("Welcome to our Java class.  Which student would you like to learn more about? (enter a number 1 -5):");
 		int x = input.nextInt();
 		
-		System.out.println("Student " + x + " is " + roster [x-1] [0]);
-	}
-}
+		System.out.println("Student " + x + " is " + roster [x-1] [0] + ". What would you like to know about " + roster [x-1] [0] + " ? "
+				+ "(enter 1 for dream job or 2 for travel):  " ); 
+		
+		
+		
+	}//psvm
+}//class
 		//String [] roster= new String[2]; //may need to adjust number
 		//String[][] roster = new String [5][3]; //create a multi array statement.  type[] [] arrayName=new type[rowCount][columnCount];
 		
